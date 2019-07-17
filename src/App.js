5311fdf5a074892a8a0e04155a254a89f1da1bc8
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-07-17 12:00:30 -0700

    Added ApodContainer component and installed axios in project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 
 function App() {
+  const [pic, setPic] = useState([]);
+
   return (
     <div className="App">
       <Header text={`Nasa's Astronomy Picture of the Day (APOD)`} />
---
+++ src/components/ApodContainer.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+export default function Header({text}) {
+    return (
+        <h1 className='apod-header'>{text}</h1>
+    )
+}
