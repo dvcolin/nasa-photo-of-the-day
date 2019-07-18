762ef38d7e92e5c641f29e2f09d0929dcbeb28ee
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-07-18 13:10:08 -0700

    Added styled components (Navbar and NavbarLogo) to App

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import "./App.scss";
 import { Header } from 'semantic-ui-react';
 import ApodContainer from "./components/ApodContainer";
 import {dateUrls} from './data'
+import {Navbar, NavbarLogo} from './components/Navbar'
 
 function App() {
 
-
   return (
     <div className="App">
+      <Navbar>
+        <NavbarLogo>NASA</NavbarLogo>
+      </Navbar>
       <Header className='apod-header' as='h1'>Nasa's Astronomy Picture of the Day (APOD)</Header>
 
       {dateUrls.map(url => <ApodContainer dateUrl={url} />)}
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Navbar = styled.div`
+    height: 70px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  `;
+
+  export const NavbarLogo = styled.div`
+    color: white;
+    margin-right: auto;
+    margin-left: 2.5rem;
+    font-size: 2.2rem;
+  `;
