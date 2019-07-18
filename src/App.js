d87ee4b1777df729286f469cf52288ee637d7f98
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-07-18 13:37:44 -0700

    Added multiple styled components in navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "./App.scss";
-import { Header } from 'semantic-ui-react';
 import ApodContainer from "./components/ApodContainer";
-import {dateUrls} from './data'
-import {Navbar, NavbarLogo} from './components/Navbar'
-
+import { dateUrls } from "./data";
+import {
+  Navbar,
+  NavbarLogo,
+  NavbarLinks,
+  NavbarLink
+} from "./components/Navbar";
+import { Card, Icon, Image, Header } from "semantic-ui-react";
 function App() {
-
   return (
     <div className="App">
       <Navbar>
         <NavbarLogo>NASA</NavbarLogo>
+        <NavbarLinks>
+          <NavbarLink href="#" status="active">
+            Home
+          </NavbarLink>
+          <NavbarLink href="#">About</NavbarLink>
+          <NavbarLink href="#">Gallery</NavbarLink>
+          <NavbarLink href="#">Contact</NavbarLink>
+        </NavbarLinks>
       </Navbar>
-      <Header className='apod-header' as='h1'>Nasa's Astronomy Picture of the Day (APOD)</Header>
+      <Header className="apod-header" as="h1">
+        Nasa's Astronomy Picture of the Day (APOD)
+      </Header>
 
-      {dateUrls.map(url => <ApodContainer dateUrl={url} />)}
+      {dateUrls.map(url => (
+        <ApodContainer dateUrl={url} />
+      ))}
     </div>
   );
 }
 
 export default App;
-
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,3 +13,20 @@ export const Navbar = styled.div`
     margin-left: 2.5rem;
     font-size: 2.2rem;
   `;
+
+  export const NavbarLinks = styled.nav`
+    width: 50%;
+    display: flex;
+    justify-content: space-around;
+  `;
+
+  export const NavbarLink = styled.a`
+    font-size: 1.2rem;
+    color: ${props => props.status === `active` ? `white` : `gray`};
+    text-transform: uppercase;
+    text-decoration: none;
+
+    :hover {
+        color: white;
+    }
+  `;
